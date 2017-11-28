a9246eadc3b9c13af88be8a8df44ff69457d6c4b
Ivan E. Garcia <mxartesano@gmail.com>
2017-11-27 21:26:09 -0600

    Creacion de proyecto

---
+++ src/java/com/mxartesano/develop/java8a/eventos/service/RegistroService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mxartesano.develop.java8a.eventos.service;
+
+import com.mxartesano.develop.java8a.eventos.model.Registro;
+
+/**
+ *
+ * @author mxartesano
+ */
+public class RegistroService {
+    public void validarSesion(Registro registro){
+        
+    }
+    
+}
