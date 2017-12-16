0f192ccd88822801471971db71f6229b7264802b
Usuario <Usuario@DESKTOP-JAKL77J>
2017-12-16 12:56:30 -0600

    se declararon las variables de usuario y los getters and setters

--- src/java/com/mxartesano/develop/java8a/eventos/model/Usuario.java
+++ src/java/com/mxartesano/develop/java8a/eventos/model/Usuario.java
@@ -10,5 +10,60 @@ package com.mxartesano.develop.java8a.eventos.model;
  * @author igarcia
  */
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correoelectronico;
+    private String password;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreoelectronico() {
+        return correoelectronico;
+    }
+
+    public void setCorreoelectronico(String correoelectronico) {
+        this.correoelectronico = correoelectronico;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
